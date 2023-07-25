f3500e91681bb8896ba0bfa6547b14c314d376b8
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-07-26 00:58:51 +0530

    integerated create functionality

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,13 @@
+import { BillBoardsClient } from "./components/client";
+
+const BillBoards = () => {
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillBoardsClient />
+      </div>
+    </div>
+  );
+};
+
+export default BillBoards;
